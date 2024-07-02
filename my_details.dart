639f079d727dd90e75f0d9706c8a05e1451d2f4a
Charminglance <safeelsaffsf@gmail.com>
2024-07-02 21:51:53 +0530

    DAY 4 TASK 1

---
+++ my_details.dart
@@ -0,0 +1,11 @@
+void main()
+{
+  String name="SAFEEL";
+  int age=19;
+  String college="MES COLLEGE OF ENGINEERING";
+  
+  print('MY DETAILS : ');
+  print("NAME : $name");
+  print("AGE : $age");
+  print("COLLEGE : $college");
+}
